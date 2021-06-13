b633c4897a1c55fea88943eacd005277fba2df20
Luciano Júnior <luciannojunior@gmail.com>
2021-06-12 21:34:20 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Source code from Programming Phoenix Liveview book
+
+I've got it [here](https://pragprog.com/titles/liveview/programming-phoenix-liveview/). Keeping on Github to ease exploration.
---
+++ core/pento/lib/pento_web/presence.ex
@@ -0,0 +1,31 @@
+#---
+# Excerpted from "Programming Phoenix LiveView",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/liveview for more book information.
+#---
+defmodule PentoWeb.Presence do
+  use Phoenix.Presence,
+    otp_app: :pento,
+    pubsub_server: Pento.PubSub
+
+  alias PentoWeb.Presence
+  alias Pento.Accounts
+  @user_activity_topic "user_activity"
+
+  def track_user(pid, product, token) do
+    user = Accounts.get_user_by_session_token(token)
+
+    case Presence.get_by_key(@user_activity_topic, product.name) do
+      [] ->
+        Presence.track(pid, @user_activity_topic, product.name, %{users: [%{email: user.email}]})
+
+      %{metas: [%{users: active_users_for_product}]} ->
+        Presence.update(pid, @user_activity_topic, product.name, %{
+          users: [active_users_for_product | %{email: user.email}]
+        })
+    end
+  end
+end
